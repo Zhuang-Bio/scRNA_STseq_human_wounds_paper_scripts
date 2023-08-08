a7e134db7f8a74a68b8d5d405aee8de1f88284b0
Zhuang Liu <55880584+Zhuang-Bio@users.noreply.github.com>
2023-08-08 17:46:13 +0200

    Add files via upload

---
+++ 01-scRNAseq Acute Wounds/01-cellranger/s1cellranger8.sh
@@ -0,0 +1,30 @@
+#! /bin/bash -l
+#SBATCH -A sens2020010
+#SBATCH -p core -n 16
+#SBATCH -t 5-00:00:00
+#SBATCH -J hs38CR8
+#SBATCH -e hs38CR8.SLURM_Job_id=%j.sderr
+#SBATCH -o hs38CR8.SLURM_Job_id=%j.sdout
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=zhuang.liu@ki.se
+
+module load bioinfo-tools cellranger/5.0.1
+
+cd /proj/sens2020010/proj_10X_woundhealing/01_cellranger
+
+JOBID="PWH27D7"
+SAMPLE_IDS="P20852_1008"
+TRANSCRIPTOME="/proj/sens2020010/ref10X_GRCh38_2021Jul/GRCh38"
+FASTQS="/proj/sens2020010/proj_10X_woundhealing/P20852/P20852_1008/02-FASTQ/210531_A00621_0413_BHVFK2DSXY"
+
+cellranger count \
+--id=$JOBID \
+--transcriptome=$TRANSCRIPTOME \
+--fastqs=$FASTQS \
+--sample=$SAMPLE_IDS \
+--localcores=16 \
+--localmem=90 \
+--expect-cells=10000
+
+/proj/sens2020010/proj_10X_woundhealing/P20852/P20852_1008/02-FASTQ/210531_A00621_0413_BHVFK2DSXY/P20852_1008_S16_L003_R1_001.fastq.gz \
+/proj/sens2020010/proj_10X_woundhealing/P20852/P20852_1008/02-FASTQ/210531_A00621_0413_BHVFK2DSXY/P20852_1008_S16_L003_R2_001.fastq.gz
